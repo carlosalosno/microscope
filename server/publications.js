80d79dcf6964a545c34809e632bec6140db56b9b
Carlos <carlos.alonso@techex.es>
2017-01-22 18:28:42 +0000

    Commit 10-4 made a simple publication/subscription for comments

--- server/publications.js
+++ server/publications.js
@@ -3,5 +3,8 @@ Meteor.publish('posts', function() {
 });
 
 Meteor.publish('comments', function() {
-    return Comments.find();
+    check(postId, String);
+    return Comments.find({
+        postId: postId
+    });
 })
