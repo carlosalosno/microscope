f3a0178424add6539b55c7b58565c52203b8f670
Carlos <carlos.alonso@techex.es>
2017-01-22 17:45:04 +0000

    comments count

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,7 +1,6 @@
-// Fixture data
+// Fixture data 
 if (Posts.find().count() === 0) {
     var now = new Date().getTime();
-
 
     // create two users
     var tomId = Meteor.users.insert({
@@ -22,7 +21,8 @@ if (Posts.find().count() === 0) {
         userId: sacha._id,
         author: sacha.profile.name,
         url: 'http://sachagreif.com/introducing-telescope/',
-        submitted: new Date(now - 7 * 2600 * 1000)
+        submitted: new Date(now - 7 * 3600 * 1000),
+        commentsCount: 2
     });
 
     Comments.insert({
@@ -41,20 +41,21 @@ if (Posts.find().count() === 0) {
         body: 'You sure can Tom!'
     });
 
-    Post.insert({
+    Posts.insert({
         title: 'Meteor',
         userId: tom._id,
         author: tom.profile.name,
         url: 'http://meteor.com',
-        submitted: new Date(now - 10 * 3600 * 1000)
+        submitted: new Date(now - 10 * 3600 * 1000),
+        commentsCount: 0
     });
 
-    Post.insert({
+    Posts.insert({
         title: 'The Meteor Book',
         userId: tom._id,
         author: tom.profile.name,
-        url: 'http://themetheorbook.com',
-        submitted: new Date(now - 12 * 3600 * 1000)
+        url: 'http://themeteorbook.com',
+        submitted: new Date(now - 12 * 3600 * 1000),
+        commentsCount: 0
     });
-
 }
